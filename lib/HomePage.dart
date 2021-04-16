e3c3689bae871d3e0e91e261e3f27fd2ab5ce469
PawankumarPK <pk9910765616@gmail.com>
2021-04-16 13:23:26 +0530

    fix await function which is not working on homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,9 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
+  List currencies;
+  HomePage(this.currencies);
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -11,19 +15,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List currencies;
   final List<MaterialColor> _color = [Colors.red, Colors.blue, Colors.indigo];
-
-  @override
-  void initState() async {
-    super.initState();
-    currencies = await getCurrencies();
-  }
-
-  Future<List> getCurrencies() async {
-    String cryptoUrl =
-        "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=de6dd73f-7bff-40c2-bdfb-1175a65f7fae";
-    http.Response response = await http.get(cryptoUrl);
-    return json.decode(response.body);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +34,40 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (BuildContext context, int index) {
         final Map currency = currencies[index];
         final MaterialColor color = _color[index % _color.length];
+        return _getListItemUi(currency, color);
       },
     )));
+  }
+
+  ListTile _getListItemUi(Map currency, MaterialColor color) {
+    return new ListTile(
+      leading: new CircleAvatar(
+        backgroundColor: color,
+        child: new Text(currency["name"][0]),
+      ),
+      title: new Text(currency["name"],
+          style: new TextStyle(fontWeight: FontWeight.bold)),
+      subtitle:
+          _getSubtitleText(currency['cmc_rank'], currency['total_supply']),
+    );
+  }
+
+  Widget _getSubtitleText(String priceUSD, String percentageChange) {
+    TextSpan priceTextWidget = new TextSpan(
+        text: "\$$priceUSD\n", style: new TextStyle(color: Colors.black));
+    String percentageChangeText = "1 hour: $percentageChange%";
+    TextSpan percentageChangeTextWidget;
+
+    if (double.parse(percentageChange) > 0) {
+      percentageChangeTextWidget = new TextSpan(
+          text: percentageChangeText,
+          style: new TextStyle(color: Colors.green));
+    } else {
+      percentageChangeTextWidget = new TextSpan(
+          text: percentageChangeText, style: new TextStyle(color: Colors.red));
+    }
+    return new RichText(
+        text: new TextSpan(
+            children: [priceTextWidget, percentageChangeTextWidget]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(myApp());
+void main() async {
+  List currencies = await getCurrencies();
+  print(currencies);
+  runApp(new myApp(currencies));
+}
 
 class myApp extends StatelessWidget {
+  final List _curriences;
+
+  myApp(this._curriences)
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: new ThemeData(
-        primarySwatch: Colors.pink
-      ),
-      home: HomePage(),
+      theme: new ThemeData(primarySwatch: Colors.pink),
+      home: HomePage(_curriences),
     );
   }
+}
+
+Future<List> getCurrencies() async {
+  String cryptoUrl =
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=de6dd73f-7bff-40c2-bdfb-1175a65f7fae";
+  http.Response response = await http.get(cryptoUrl);
+  return json.decode(response.body);
 }
